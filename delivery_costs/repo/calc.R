d4c636d2f82f64ae52a53a6ece806578813de372
Joey Frostad <jfrostad@ihme.washington.edu>
2023-03-24 13:24:21 -0700

    checking in a first version of the working LF and deliv costs code before i make schisto updates

--- delivery_costs/repo/calc.R
+++ delivery_costs/repo/calc.R
@@ -34,6 +34,7 @@ setwd(my_repo)
 #TODO cleanup here
 pacman::p_load(readxl, janitor, data.table, naniar, dplyr, stringr, magrittr, #data wrangling
                scales, ggplot2, ggridges, ggrepel, gridExtra, RColorBrewer, viridis, #viz
+               plm, lme4, #modelling tools
                stargazer, #sig tables
                #caret, mlbench, randomForest, pls, #ML tools
                zoo)
@@ -43,10 +44,14 @@ pacman::p_load(readxl, janitor, data.table, naniar, dplyr, stringr, magrittr, #d
 # ----IN/OUT------------------------------------------------------------------------------------------------------------
 ###Input###
 code_dir <- file.path(my_repo, 'code')
+data_dir <- file.path(local_dir, 'data')
 
 #TODO consider which data is universal and which is by disease. How to organize
 model_data_dir <- file.path(my_dropbox, '/1 - Data gathering - all diseases/Costs/Delivery costs/')
   coef_path <- "Delivery_cost_regression_coefficients_to_use_7_12_2022.csv" #TODO rename
+  
+#also load the central model objs received from fitzpatrick
+load(file.path(data_dir, 'benchmark.Rdata'), verbose=T)
   
 #ihme data
 ihme_version <- '2022-09-13'
@@ -54,12 +59,21 @@ ihme_data_dir <- file.path(my_dropbox, 'IHME', ihme_version)
   pop_path <- 'IU_pop_dens_all_age.csv'
   
 #ntd mc data
+mc_version <- '2023-02-24'
 mc_data_dir <- file.path(my_dropbox, 'LF/NTD MC')
 
 #TODO for now we are just doing this for LF. in the future should be a function with disease type as an argument
 lf_version <- '2022-08-26'
 lf_data_dir <- file.path(my_dropbox, paste0('LF/InputData ', lf_version))
   lf_path <- 'MDA_costs_22_08_2022_AM.csv'
+
+  
+#also some IU level data for LF
+  lf_iu_version <- '2023-02-15'
+  lf_iu_data_dir <- file.path(my_dropbox, paste0('LF/InputData ', lf_iu_version))
+  lf_iu_path <- 'lf_long_IU_cov.csv' #coverage data at IU scenario level
+  lf_iu_dens_path <- 'IU_pop_dens.csv' #population data at IU year level
+  lf_iu_int_path <- 'MDA-procurement-cost_LF.xlsx' #disease integration structure
   
   # example_path <- "Example data/Example_data_for_testing_code_7_12_2022.csv"
   # cost_path <- 'Cost_data_23_08_2022_complete_to_share.csv'
@@ -101,7 +115,7 @@ if(reload) {
     .[, espen_loc := paste0(iso, '0', iu_id)]
   
   #ntd mc dataset which we will filter the IUs by
-  iu_dt <- file.path(mc_data_dir) %>% 
+  iu_dt <- file.path(mc_data_dir, mc_version, 'Data') %>% 
     list.files(full.names = T) %>% 
     lapply(., fread, select=c('espen_loc')) %>% 
     rbindlist %>% 
@@ -124,21 +138,31 @@ if(reload) {
   lf_dt <- file.path(lf_data_dir, lf_path) %>% 
     fread %>% 
     setnames(., names(.), names(.) %>% tolower) %>%  #force names to lowercase for consistency 
+    unique(by=c('iso')) %>%  #collapse the scenarios, we will use another datasheet
+    .[, scenario := NULL] %>% 
+    .[, c('iso', 'gdp', 'delivery_cost_range')] %>%  #this is all we need for now
+    #TODO eventually should just retire this dataset as gdp is the only use of it now
     #impute the IMP decision constants
     #TODO, validate these assumptions and eventually put them all into an assumption sheet to merge on?
     .[, eco := 0] %>% 
     .[, vol := 0] %>% 
-    .[, int := 1] %>% 
+    #.[, int := 1] %>% #this is now country/cov specific
     .[, rds := 1] %>%  #TODO should be scenario specific but it's not present
     .[, yrs := 5] %>% 
-    .[, nat := 1] %>%  #TODO could invert subnat but its the same assumption
-    .[, sch := 1] %>% 
-    .[, pop := str_replace_all(pop, ',', '') %>% as.numeric] %>% #convert from chr to numeric
-    .[, pop_treated := str_replace_all(`mda size`, ',', '') %>% as.numeric] %>% #convert from chr to numeric
+    .[, nat := 0] %>%  #TODO could invert subnat but its the same assumption
+    .[, sch := 0] %>% 
+    #.[, den := pop_density] %>% #match the naming in fitzpatrick model
+    #.[, pop := str_replace_all(pop, ',', '') %>% as.numeric] %>% #convert from chr to numeric
+    #.[, pop_treated := str_replace_all(`mda size`, ',', '') %>% as.numeric] %>% #convert from chr to numeric
     .[, gdp := str_replace_all(gdp, ',', '') %>% as.numeric] %>% #convert from chr to numeric
+    .[, VUT := 0] %>% #create study level dummies and set to 0
+    .[, Kri := 0] %>% #create study level dummies and set to 0
+    .[, Fri := 0] %>% #create study level dummies and set to 0
+    .[, Mon := 0] #create study level dummies and set to 0
     #TODO not clear what to do with unit percentages...not given as decimals in table 2
-    .[, cov := str_replace(coverage, '%', '') %>% as.numeric] %>% 
-    .[, cov := cov/1] #need to confirm this is the right way to unit the percentages
+    #.[, cov := str_replace(coverage, '%', '') %>% as.numeric] %>% 
+    #.[, cov := cov/1] #need to confirm this is the right way to unit the percentages
+  
   
   #there are 6 countries some missing pops/mda size/GDPs so i will impute them using world bank figures
   lf_dt[iso=='BDI', gdp:= 221.48]
@@ -148,9 +172,39 @@ if(reload) {
   lf_dt[iso=='SWZ', gdp:= 3978.4]
   lf_dt[iso=='TZZ', gdp:= 1031] #zanzibar: i didnt see this split out on the world bank site so i used UN figures here:
   #http://data.un.org/Data.aspx?d=SNAAMA&f=grID:101;currID:USD;pcFlag:1;crID:836
-  
+
   #add quintiles of gdp
   lf_dt[, gdp_quint := cut(gdp, quantile(gdp, probs=0:5/5, na.rm=T), include.lowest=TRUE, labels=FALSE)]
+  
+  
+  #they are also missing pop/pop densities, using the mean by GDP for now
+  # lf_dt[, pop := impute_mean(pop), by=gdp_quint] #missing densities for some countries (see below)
+  # lf_dt[, den := impute_mean(den), by=gdp_quint] #missing densities for some countries (see below)
+  
+  #bring in the IU level data and merge it on to expand and calculate pop treated
+  #iu/scenario specific coverage
+  lf_iu_cov_dt <- file.path(lf_iu_data_dir, lf_iu_path) %>% 
+    fread %>% 
+    setnames('value', 'cov') %>% 
+    .[, ]
+  #iu level population data
+  lf_dens_dt <- file.path(lf_iu_data_dir, lf_iu_dens_path) %>% 
+    fread
+  #disease integration data at country level
+  lf_int_dt <- file.path(lf_iu_data_dir, lf_iu_int_path) %>% 
+    read_excel() %>% 
+    as.data.table() %>% 
+    .[, .(ISO, `Co-endemic`)] %>% 
+    unique() %>% #TODO verify that theres no difference in integration between the different Product/Drug groups (there are 2 diff)
+    .[, int := 1] %>% 
+    .[`Co-endemic`==1, int := 2]
+  
+  #expand to the IU level and add coverage/density
+  lf_dt <- merge(lf_dt, lf_iu_cov_dt, by.x=c('iso'), by.y=c('ISO'))
+  lf_dt <- merge(lf_dt, lf_dens_dt, by=c('IU_ID', 'year_id')) #TODO verify correct year
+  lf_dt <- merge(lf_dt, lf_int_dt, by.x=c('iso'), by.y=c('ISO'))
+  lf_dt[, pop_treated := cov*adj_pop]#calculate the target pop as cov*IU pop
+  lf_dt[, pop_density := dens]
   
 }
 
@@ -198,7 +252,7 @@ fake_dt <- data.table(eco=rnorm(n = 1000, mean = 0, sd = 1),
                       cost=rnorm(n = 1000, mean = 0, sd = 1))
 
 #setup a fake lm model that will store our extracted coefs
-mod <- lm(cost~eco+vol+log(int)+log(rds)+
+fake_mod <- lm(cost~eco+vol+log(int)+log(rds)+
             yrs+cov+nat+sch+log(pop_treated)+log(pop_density)+log(gdp)+
             eco:log(int)+cov:nat+eco:sch+cov:sch+
             nat:log(pop_treated)+nat:log(pop_density)+nat:log(gdp),
@@ -206,7 +260,7 @@ mod <- lm(cost~eco+vol+log(int)+log(rds)+
 
 #then inject our literature coefficients
 #TODO setup some tests to assert that model formula matches coef dt order
-coef_names <- mod$coefficients %>% names #extract the names for clarity because injecting erases them
+coef_names <- fake_mod$coefficients %>% names #extract the names for clarity because injecting erases them
 #also make a vector of the ivs for later examination
 coef_vars <- coef_names %>% 
   .[!(.%like%':')] %>% 
@@ -214,8 +268,109 @@ coef_vars <- coef_names %>%
   str_remove_all(., "log\\(") %>% 
   str_remove_all(., "\\)")
 #inject coefficients from the extraction dt into the fake model obj
-mod$coefficients <- unlist(coef_dt[1]) %>% as.numeric() 
-names(mod$coefficients) <- coef_names #preserve the names
+fake_mod$coefficients <- unlist(coef_dt[1]) %>% as.numeric() 
+names(fake_mod$coefficients) <- coef_names #preserve the names
+
+#start building a new model for IU level predicting
+setDT(mda_uc)[, pop_treated := pop] %>%  #set to match our naming convention
+  .[, pop_density := den] %>%  #set to match our naming convention
+  .[, cov := cov/100] #treat as a percent so we can test logit transforms
+
+
+#lets examine some of the values
+
+#split out vars so we can melt and compare
+dummy_vars <- c('eco', 'vol', 'sch', 'nat')
+cont_vars <- c('int', 'rds', 'yrs', 'cov', 'pop_treated', 'pop_density', 'gdp') #we will plot only the continuous vars
+plot_dt <- melt(mda_uc,
+                measure.vars=cont_vars,
+                variable.name = 'dep_var',
+                value.name = 'dep_value')
+
+#look at all the distributions of our data separately
+#check out the distributions of natl values and then the subnational ones by country
+varDistPLot <- function(var, dt, by_var=NULL, trans='none') {
+  message(var)
+  plot_dt <- copy(dt)
+  if(trans=='log') plot_dt[, (var) := var %>% get %>% log]
+  if(trans=='logit') plot_dt[, (var) := var %>% get %>% logit]
+  plot <-
+  ggplot(plot_dt, aes_string(x=var, fill=by_var, group=by_var)) +
+    geom_density(alpha=0.4) +
+    ggtitle("Variable Distributions", subtitle=paste0('Data Trans: ', trans)) +
+    scale_fill_viridis_c() +
+    theme_minimal()
+ 
+}
+
+pdf(file.path(viz_dir, 'coef_distributions.pdf'), height=8, width=12)
+lapply(cont_vars, varDistPLot, dt=mda_uc, by_var='nat')
+lapply(cont_vars, varDistPLot, dt=mda_uc, by_var='nat', trans='log')
+varDistPLot(var='cov', dt=mda_uc, by_var='nat', 'logit') %>% print
+varDistPLot(var='ucb', dt=mda_uc, by_var='nat') %>% print
+varDistPLot(var='ucb', dt=mda_uc, by_var='nat', trans='log') %>% print
+dev.off()
+
+#transformations
+plot_dt[dep_var %in% c('int', 'rds', 'pop_treated', 'pop_density', 'gdp'), dep_value := dep_value]
+plot_dt[, dep_value_trans := dep_value]
+plot_dt[dep_var %in% c('int', 'rds', 'pop_treated', 'pop_density', 'gdp'), dep_value_trans := log(dep_value)]
+plot_dt[dep_var=='cov', dep_value_trans := logit(dep_value)]
+
+#look at all the variable relationships with our IV
+ggplot(plot_dt[ucb<8], aes(x=dep_value, y=ucb, color=nat, group=nat)) +
+  geom_point(alpha=.8) +
+  facet_wrap(~dep_var) +
+  scale_y_log10() +
+  scale_color_viridis() +
+  theme_minimal()
+
+#look at all the variable relationships with our IV if we transform the vars
+ggplot(plot_dt[ucb<8], aes(x=dep_value_trans, y=log(ucb), color=nat, fill=nat, group=nat)) +
+  geom_point(alpha=.8) +
+  geom_smooth(method='loess') +
+  facet_wrap(~dep_var) +
+  scale_y_log10() +
+  scale_color_viridis() +
+  scale_fill_viridis(guide=F) +
+  theme_minimal()
+
+#examine some vars more closely
+ggplot(mda_uc[ucb<8], aes(x=log(pop_density)*log(pop_treated), y=ucb)) +
+  geom_point(aes(alpha=log(pop_treated))) +
+  geom_smooth(method='gam', color='darkorange1') +
+  facet_wrap(~sch) +
+  scale_y_log10() +
+  scale_color_viridis() +
+  theme_minimal()
+
+#rerun the fitzpatrick model
+og_mod <- lmer(log(ucb)~eco+vol+log(int)+log(rds)+
+              yrs+cov+nat+sch+log(pop_treated)+log(den)+log(gdp)+
+              VUT+Kri+Fri+Mon+
+              eco:log(int)+cov:nat+eco:sch+cov:sch+
+              nat:log(pop_treated)+nat:log(den)+nat:log(gdp) +(1|study_ref),
+            data=mda_uc)
+
+#run our version of the model
+mod <- lmer(log(ucb)~eco+vol+log(int)+log(rds)+yrs+logit(cov)+sch+
+              log(pop_treated)*log(pop_density)+log(gdp)+
+              VUT+Kri+Fri+Mon+
+              eco:log(int)+eco:sch+sch:logit(cov)+
+              (1|study_ref),
+            data=mda_uc[nat==0])
+
+nat_mod <- lmer(log(ucb)~eco+vol+log(int)+log(rds)+yrs+logit(cov)+sch+
+              log(pop_treated)+log(den)+log(gdp)+
+              VUT+Kri+Fri+Mon+
+              eco:log(int)+eco:sch+cov:sch+
+              (1|study_ref),
+            data=mda_uc[nat==1])
+
+#output the model results
+stargazer(mod,
+          type='html') %>%
+  capture.output(file=file.path(out_dir, 'table_1.html'))
 
 #***********************************************************************************************************************
 
@@ -228,32 +383,57 @@ names(mod$coefficients) <- coef_names #preserve the names
 #TODO test a few rows manually
 
 #make national predictions
-lf_dt[, cost_natl := predict(mod, newdata =lf_dt) %>% exp]
+#lf_dt[, cost_natl := predict(mod, newdata =lf_dt, re.form=NA) %>% exp]
 
 #merge with pop density data to breakdown into IUs
-setnames(lf_dt, c('pop_density'), c('pop_density_iso'))
-lf_dt <- merge(lf_dt, pop_dt, by='iso', all.x=T, allow.cartesian = T)
+#setnames(lf_dt, c('pop_density'), c('pop_density_iso'))
+#lf_dt <- merge(lf_dt, pop_dt, by='iso', all.x=T, allow.cartesian = T)
+#lf_dt[, den := pop_density] #match the naming conventions in fitzpatrick model
+#lf_dt[, den := impute_mean(den), by=gdp_quint] #TODO missing densities for some IUs, use avg by GDP
 #setnames(lf_dt, 'adj_pop', 'pop') #TODO confirm that this is the right value
 
 #generate also the IU cost
 #should we be changing nat to 0 now that these are subnational sites?
 lf_dt[, nat := 0] #if we do it significantly drops the cost to more reasonable values
-lf_dt[, cost_iu := predict(mod, newdata =lf_dt) %>% exp]
+lf_dt[, sch := 0]
+lf_dt[gdp>max(mda_uc$gdp, na.rm=T), gdp := max(mda_uc$gdp, na.rm=T)] #cap GDP to maximum value in dataset
+lf_dt[, cost_iu := predict(mod, newdata =lf_dt, re.form=NA) %>% exp]
 
 #output the results
-write.csv(lf_dt, file=file.path(lf_out_dir, 'delivery_cost_preds.csv'))
-
+write.csv(lf_dt[, c('iso', 'scenario', 'year_id', 'IU_ID', coef_vars, 'cost_iu'), with=F], 
+          file=file.path(lf_out_dir, 'delivery_cost_preds_integrated.csv'))
 #***********************************************************************************************************************
 
 # ---DIAGNOSTICS--------------------------------------------------------------------------------------------------------
 ##examine some rows and test them against the online shiny tool
 #this line can be used interactively to test different country and IU combinations in a compact manner
-lf_dt[, c('iso', 'iu_id', coef_vars, 'pop_density_iso', 'cost_natl', 'cost_iu'), with=F]
+lf_dt[iso=="NGA", c('iso', 'IU_ID', coef_vars, 'cost_iu'), with=F]
 
 #highlight outliers for plots
 lf_dt[, iso_label := ifelse(isOutlier(cost_iu), iso, NA_character_)]
 lf_dt[is.na(iso_label), iso_label:="All Others"]
 
+#see which variables are driving outliers
+plot_dt <- melt(lf_dt,
+                measure.vars=cont_vars,
+                variable.name = 'dep_var',
+                value.name = 'dep_value')
+plot_dt[dep_var %in% c('int', 'rds', 'pop_treated', 'pop_density', 'gdp'), dep_value := dep_value]
+plot_dt[, dep_value_trans := dep_value]
+plot_dt[dep_var %in% c('int', 'rds', 'pop_treated', 'pop_density', 'gdp'), dep_value_trans := log(dep_value)]
+plot_dt[dep_var=='cov', dep_value_trans := logit(dep_value)]
+
+
+#scatter to see the relationships
+ggplot(plot_dt[cost_iu>3], aes(x=dep_value_trans, y=cost_iu, color=gdp_quint)) +
+  geom_point(alpha=.8) +
+  #geom_smooth(method='loess') +
+  facet_wrap(~dep_var) +
+  scale_y_log10() +
+  scale_color_viridis() +
+  #scale_fill_viridis(guide=F) +
+  theme_minimal()
+file.path(viz_dir, 'coef_v_iu_costs.png') %>% ggsave(height=8, width=12)
 
 #scatter the IU preds against the natl preds to look for outliers
   ggplot(lf_dt, aes(cost_iu, cost_natl, color=iso_label, size=gdp)) +
@@ -285,10 +465,11 @@ file.path(viz_dir, 'natl_cost_distributions.png') %>% ggsave(height=8, width=12)
 #subnatl, binned by gdp quintiles
 subnatlDensPlot <- function(i, var, dt) {
   message(var)
-  ggplot(dt[gdp_quint%in%i], aes_string(var, fill='iso')) +
+  ggplot(dt[gdp_quint%in%i & year_id %in%seq(2020,2040,5)], aes_string(var, fill='iso')) +
     geom_density(alpha=0.65) +
     scale_x_log10()+
     scale_fill_brewer('Country', palette = 'Set1') +
+    facet_wrap(~year_id)+
     ggtitle(paste0("Delivery Cost Distributions for GDP Quintile #", i)) +
     theme_minimal()
 }
